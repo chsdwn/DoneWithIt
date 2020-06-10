09f6d42b1f20f3548ee5aa2707844de06045eb67
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-06-10 15:58:56 +0300

    2-Flatlist

---
+++ src/screens/MessagesScreen.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+import { ListItem } from '../components/ListItem';
+
+const messages = [
+  {
+    id: 1,
+    title: 'T1',
+    description: 'D1',
+    image: require('../assets/user.png'),
+  },
+  {
+    id: 2,
+    title: 'T2',
+    description: 'D2',
+    image: require('../assets/user.png'),
+  },
+];
+
+export const MessagesScreen = () => {
+  return (
+    <FlatList
+      data={messages}
+      renderItem={({ item }) => (
+        <ListItem
+          title={item.title}
+          subTitle={item.description}
+          image={item.image}
+        />
+      )}
+    />
+  );
+};
+
+const styles = StyleSheet.create({});
