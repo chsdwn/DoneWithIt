5699a360190e4f0069bf56b0c8ad72f3b1a394f1
Hulusi Kafalıer <hulusikafalier@gmail.com>
2020-06-26 18:20:15 +0300

    6-Accesssing the Library

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
-import React, { useEffect } from 'react';
-import { Alert, StyleSheet, StatusBar, SafeAreaView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  Alert,
+  Button,
+  Image,
+  StyleSheet,
+  StatusBar,
+  SafeAreaView,
+} from 'react-native';
+import ImagePicker, { ImagePickerOptions } from 'react-native-image-picker';
 import { check, PERMISSIONS, RESULTS } from 'react-native-permissions';
 
 import { ListingEditScreen } from './src/screens/ListingEditScreen';
 import { LoginScreen } from './src/screens/LoginScreen';
 
+const imagePickerOptions: ImagePickerOptions = {
+  title: 'Select Photo',
+  storageOptions: {
+    skipBackup: true,
+    path: 'images',
+  },
+};
+
 export const App = () => {
+  const [imageUri, setImageUri] = useState<string | undefined>(undefined);
+
   useEffect(() => {
     check(PERMISSIONS.ANDROID.CAMERA)
       .then((result) => {
@@ -26,11 +44,25 @@ export const App = () => {
       .catch((error) => console.log);
   }, []);
 
+  const selectImage = () => {
+    ImagePicker.showImagePicker(imagePickerOptions, (response) => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+        setImageUri(undefined);
+      } else if (response.error) {
+        console.log('ImagePicker error, ', response.error);
+      } else {
+        setImageUri(response.uri);
+      }
+    });
+  };
+
   return (
     <>
       <StatusBar />
       <SafeAreaView style={styles.container}>
-        <ListingEditScreen />
+        <Button title="Image Picker" onPress={selectImage} />
+        <Image source={{ uri: imageUri }} style={{ width: 640, height: 480 }} />
       </SafeAreaView>
     </>
   );
